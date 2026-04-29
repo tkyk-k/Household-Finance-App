cdf7eef6a5648aacc304347f9de15e56b7845c16
tkyk-k <tkyk.koyama@gmail.com>
2026-04-29 12:30:33 +0900

    add

--- backend/main.py
+++ backend/main.py
@@ -19,7 +19,6 @@ def get_token(request: Request):
 async def get_assets(request: Request):
     token = get_token(request)
 
-    # Viewを取得
     res = requests.get(
         f"{SUPABASE_URL}/rest/v1/current_assets",
         headers={
@@ -27,30 +26,51 @@ async def get_assets(request: Request):
             "Authorization": f"Bearer {token}",
         }
     )
-    
-    print(res.text)
 
-    data = res.json()[0]  # 1レコードだけ返る想定
+    data_json = res.json()
 
-    total = data["total_assets"]
+    if not data_json:
+        raise HTTPException(status_code=404, detail="No data")
+
+    data = data_json[0]
+
+    total_assets = data["total_assets"]
     total_prev = data["total_prev"]
     total_jan = data["total_jan"]
 
     # 差分
-    diff_from_last_month = total - total_prev
-    diff_from_jan = total - total_jan
+    diff_from_last_month = total_assets - total_prev
+    diff_from_jan = total_assets - total_jan
 
-    # 割合
-    # for u in data["users"]:
-    #     user_total = u["total"]
+    # 全体割合
+    total_cash = data["total_cash"]
+    total_investment = data["total_investment"]
 
-    #     u["ratio"] = user_total / total if total else 0
-    #     u["cash_ratio"] = u["cash"] / user_total if user_total else 0
-    #     u["investment_ratio"] = u["investment"] / user_total if user_total else 0
+    cash_ratio = total_cash / total_assets if total_assets else 0
+    investment_ratio = total_investment / total_assets if total_assets else 0
+
+    # ユーザー割合
+    users = data["users"]
+    for u in users:
+        user_total = u["total"]
+        u["ratio"] = user_total / total_assets if total_assets else 0
 
     return {
         "counted_at": data["counted_at"],
-        "total_assets": total,
+        "total_assets": total_assets,
+
+        # 差分
         "diff_from_last_month": diff_from_last_month,
         "diff_from_jan": diff_from_jan,
+
+        # 内訳
+        "total_cash": total_cash,
+        "total_investment": total_investment,
+
+        # 割合
+        "cash_ratio": cash_ratio,
+        "investment_ratio": investment_ratio,
+
+        # ユーザー
+        "users": users
     }
